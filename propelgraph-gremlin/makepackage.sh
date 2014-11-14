14d6ae2f4644b4979e77c10bce09056d2daa78e3
Jason Crawford <ccjason@us.ibm.com>
2014-11-14 17:25:22 -0500

    added support for creating a gremlin.bat and propelgraph.zip but the makepackage.sh script still runs on *nix only

--- propelgraph-gremlin/makepackage.sh
+++ propelgraph-gremlin/makepackage.sh
@@ -16,4 +16,13 @@ cp -r src/main/packaging/* ${GDIR}/
 #mkdir ${GDIR}/doc
 #cp -r src/main/doc/* ${GDIR}/doc/
 tar -cvzf ${GDIR}.tar.gz  ${GDIR}
-#cp ${GDIR}.tar.gz /var/www/html/xxx/...
+
+# the following code creates a Windos compatible gremlin.bat file and then propelgraph.zip
+cd  ${GDIR}
+TT=$(echo lib/*.jar | sed 's/ /;/g')
+echo java -cp $TT com.ibm.research.systemg.gremlin.groovy.console.Console "\1 \2 \3 \4 \5" >bin/gremlin.bat
+cd ..
+zip -r propelgraph.zip ${GDIR}
+
+#cp ${GDIR}.tar.gz  /var/www/html/xxx/...
+#cp propelgraph.zip /var/www/html/xxx/...
